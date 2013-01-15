8757ad5f045b025fa049f2b01075619404bf34b4
Alexey Belkevich <belkevich.alexey@gmail.com>
2013-01-15 20:47:47 +0200

    Initial commit. Added empty source files

---
+++ ABMultiton.h
@@ -0,0 +1,13 @@
+//
+//  ABMultiton.h
+//  RequestManagerApp
+//
+//  Created by Alexey Belkevich on 1/13/13.
+//  Copyright (c) 2013 okolodev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABMultiton : NSObject
+
+@end
---
+++ ABMultiton.m
@@ -0,0 +1,13 @@
+//
+//  ABMultiton.m
+//  RequestManagerApp
+//
+//  Created by Alexey Belkevich on 1/13/13.
+//  Copyright (c) 2013 okolodev. All rights reserved.
+//
+
+#import "ABMultiton.h"
+
+@implementation ABMultiton
+
+@end
