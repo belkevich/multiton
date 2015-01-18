5685e3c3c9baf4740a2c45f4db3f82b64cd90d40
Denys Telezhkin <strangervir@gmail.com>
2015-01-18 15:04:29 +0200

    add Swift support for singletons, that inherit from NSObject

---
+++ Spec/ABMultitonSpec/Mocks/SwiftSingleton.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftSingleton.swift
+//  ABMultiton
+//
+//  Created by Denys Telezhkin on 18.01.15.
+//  Copyright (c) 2015 Okolodev. All rights reserved.
+//
+
+import UIKit
+
+class SwiftSingleton: NSObject, ABMultitonProtocol {
+   
+}
